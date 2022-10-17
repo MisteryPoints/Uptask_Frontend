e2d425b55c882beef6767f8837347587a31689a7
MisteryPoints <misterypoint2@gmail.com>
2022-10-17 15:15:53 -0400

    Personalize

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+const Footer = () => (
+    <footer className="bg-gradient-to-r from-black to-red-600 block fixed left-0 right-0 bottom-0 p-[0.5rem] border-t-4 border-solid border-white">
+        <h2 className="text-center text-white m-0 font-bold font-sans text-xl">Desarrollado por DevPoint Víctor Tejada</h2>
+    </footer>
+)
+
+export default Footer
